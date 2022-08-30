5ede91fc2d1389ced9828dd985bc5c3e3c8a8c08
Ivan Roschin <ivan.roschin86@gmail.com>
2022-08-31 00:30:01 +0300

    update 8.2
    add currentTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+const CurrentTime = localStorage.getItem(STORAGE_KEY);
+
+const onPlay = function (data) {
+  const timeupdate = data.seconds;
+  console.log(timeupdate);
+  localStorage.setItem(STORAGE_KEY, timeupdate);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(CurrentTime)
+  .then(function (seconds) {
+    localStorage.removeItem(STORAGE_KEY);
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
