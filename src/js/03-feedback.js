dd581b8ac0f809e71cce8036554b3fac7bdda241
Ivan Roschin <ivan.roschin86@gmail.com>
2022-09-03 14:21:21 +0300

    update 8.3.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,21 +6,16 @@ const refs = {
   email: document.querySelector('.feedback-form input'),
 };
 
-console.log(refs.textarea);
-
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 
-// throttle(onFormInput, 500),
+populateFormData();
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', e => {
   formData[e.target.name] = e.target.value;
-  console.log('formData', formData);
 });
 refs.form.addEventListener('input', throttle(onFormInput, 500));
-
-populateFormData();
 
 function onFormInput() {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
@@ -29,22 +24,21 @@ function onFormInput() {
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  console.log('Отправляем форму');
-
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
   console.log('Значение объекта с данными', formData);
+  evt.currentTarget.reset();
+  for (key in formData) {
+    delete formData[key];
+  }
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function populateFormData() {
   const savedData = localStorage.getItem(STORAGE_KEY);
 
   const parsedData = JSON.parse(savedData);
-  console.log(parsedData);
 
-  if (parsedData) {
-    console.log(STORAGE_KEY);
-    refs.textarea.value = parsedData.message;
-    refs.email.value = parsedData.email;
+  if (savedData) {
+    refs.textarea.value = parsedData.message || '';
+    refs.email.value = parsedData.email || '';
   }
 }
